77fb938e1a58c86a069bfeb9a10d392738942322
SaadUghratdar <saadughratdar099@gmail.com>
2024-03-24 13:16:14 +0530

    NEW CSS

--- src/components/masterSupp/MasterSupp.jsx
+++ src/components/masterSupp/MasterSupp.jsx
@@ -1,21 +1,22 @@
 import React, {useState }  from 'react';
 import './supp.css'
 const MasterSupp = () => {
-  const [suppInputs, setSuppInputs] = useState({ suppName: '', phoneNum: '', sales:''});
+  const [suppInputs, setSuppInputs] = useState({ suppName: '', phoneNum: '', sales:'0'});
   const [suppliers, setSuppliers] = useState([]);
 
   const handleAddSupplier = (e) => {
     e.preventDefault();
-    if (suppInputs.suppName.trim() !== '' && suppInputs.phoneNum !== '') {
+    // if (suppInputs.suppName.trim() !== '' && suppInputs.phoneNum !== '') {
       setSuppliers([...suppliers, suppInputs]);
       setSuppInputs({ suppName: '', phoneNum: '', });
-    } else {
-      // Notify the user that all fields are required
-      alert('Please fill in all fields');
-    }
+    // } else {
+    //   // Notify the user that all fields are required
+    //   alert('Please fill in all fields');
+    // }
   };
 
   return (
+    <div class="scrollableTable">
     <div className="tableWrapper">
     <div className='dateDiv'>
         DATE
@@ -36,13 +37,13 @@ const MasterSupp = () => {
           value={suppInputs.phoneNum}
           onChange={(e) => setSuppInputs({ ...suppInputs, phoneNum: e.target.value })}
         />
-        <input
-          className='piecesInput'
+        {/* <input
+          className='salesInput'
           type="number"
           placeholder="Pieces"
           value={suppInputs.pieces}
           onChange={(e) => setSuppInputs({ ...suppInputs, pieces: e.target.value })}
-        />
+        /> */}
         <button type="submit">ADD</button>
       </form>
     </div>
@@ -51,8 +52,8 @@ const MasterSupp = () => {
       <thead>
         <tr>
           <th className='suppCol'>Supplier</th>
-          <th className='boxCol'>Contact</th>
-          <th className='pieceCol'>Pieces</th>
+          <th className='numCol'>Contact</th>
+          <th className='salesCol'>Sales</th>
           <th className='actCol'>Action</th>
         </tr>
       </thead>
@@ -61,7 +62,7 @@ const MasterSupp = () => {
           <tr key={index}>
             <td>{supplier.suppName}</td>
             <td>{supplier.phoneNum}</td>
-            <td>{supplier.pieces}</td>
+            <td>{supplier.sales}</td>
             <td>
             <button type="submit">EDIT</button>
             <button type="submit">DELETE</button>
@@ -70,6 +71,7 @@ const MasterSupp = () => {
         ))}
       </tbody>
     </table>
+  </div>
   </div>
   )
 }
